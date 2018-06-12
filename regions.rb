4ac64df8db51a38ccfe80b32ec994e7513873ced
TECH-A52 <tech-a52@TECH-A52s-MacBook-Air.local>
2018-06-12 20:46:18 +0100

    Regions Class complete

---
+++ regions.rb
@@ -0,0 +1,36 @@
+require_relative "pokeworld.rb"
+
+class Regions < Pokeworld
+
+  def self.kanto
+    puts 'The Kanto region is home to a lot of Pokémon and has a rich history of creating Pokémon with technology.'
+  end
+
+  def self.johto
+    puts 'Approximately 700 years before modern times, the people of Ecruteak built two nine-tier towers with the hopes of fostering friendship and hope between Pokémon and humans.'
+  end
+
+  def self.hoenn
+    puts 'Hoenn was created after Primal Groudon and Primal Kyogre were formed. Primal Groudon raised the landmasses and Primal Kyogre filled the seas that would later become Hoenn.'
+  end
+
+  def self.sinnoh
+    puts 'According to several Sinnoh myths, this region was the first of all the regions in the Pokémon world to have been created. In a void of nothingness, a single Egg came into being, which then hatched into Arceus, the first Pokémon to be in existence.'
+  end
+
+  def self.unova
+    puts 'In mythology, Unova was created by uniting the warring peoples of the land by twin heroes. They used a single dragon sometime more than 2500 years ago.'
+  end
+
+  def self.kalos
+    puts '3000 years prior to the events of Pokémon X and Y, the Kalos war occurred, the ultimate weapon was created underneath Geosenge Town, and AZ was given eternal life.'
+  end
+
+  def self.alola
+    puts 'It a ripoff hawaii'
+  end
+
+end
+
+Regions.kanto
+Regions.alola
