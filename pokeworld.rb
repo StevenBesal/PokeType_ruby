b5c9ab283f66f943a1d18998876509cea619eb15
TECH-A52 <tech-a52@TECH-A52s-MacBook-Air.local>
2018-06-12 20:09:17 +0100

    Head class complete

---
+++ pokeworld.rb
@@ -0,0 +1,9 @@
+class Pokeworld
+
+  def self.poke
+
+  puts "Pokémon are living creatures that inhabit the Pokémon world, living alongside, and usually helping, humans. Pokémon have been seen within this world as being both supplements and replacements for animals and plants that exist within the real world."
+
+  end
+
+end
